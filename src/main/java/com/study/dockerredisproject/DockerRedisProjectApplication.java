7c37420de103c848f2d12dff2e9c43a4d833a98b
ekal0323 <ekal0323@iabacus.co.kr>
2024-04-04 17:23:04 +0900

    [feature] init

---
+++ src/main/java/com/study/dockerredisproject/DockerRedisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.dockerredisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRedisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerRedisProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/dockerredisproject/DockerRedisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.dockerredisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerRedisProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
